aabd6c230cd050a7c5e961bcd78266f6ff4425cd
Alan Santana <119264746+alannzio@users.noreply.github.com>
2023-10-23 14:39:05 -0600

    Create funcionar.js

---
+++ ProyectI3/funcionar.js
@@ -0,0 +1,21 @@
+function login() {
+  var user = document.getElementById("usuario").value;
+  var pass = document.getElementById("contraseña").value;
+
+  if (user === "AumaChi" && pass === "Reportes2024Boc") {
+    window.location.href = "home.html";
+  } else if (user === "AumaQro" && pass === "Reportes2024Boc") {
+    window.location.href = "home.html";
+  } else if (user === "AumaSal" && pass === "Reportes2024Boc") {
+    window.location.href = "home.html";
+  } else if (user === "AumaSlp" && pass === "Reportes2024Boc") {
+    window.location.href = "home.html";
+  } else if (user === "ComLerma" && pass === "Reportes2024Boc") {
+    window.location.href = "home.html";
+  } else if (user === "AumaCoy" && pass === "Reportes2024Boc") {
+    window.location.href = "home.html";
+  } else {
+    alert("Por favor ingresa un nombre de usuario y contraseña válidos.");
+    console.log()
+  }
+}
